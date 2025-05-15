8e9dd7dc0c4d0b73c46771ea2fdd51cb0db83a4d
Yaroslav Kuznetsov <I-love-linux-12-31@users.noreply.github.com>
2025-05-15 23:50:07 +0300

    -

--- db.py
+++ db.py
@@ -25,7 +25,7 @@ def global_init():
         file_path = os.environ.get("DB_FILE_PATH", "/tmp/db.sqlite3")
         conn_str = f"sqlite:///{file_path}?check_same_thread=False"
     else:
-        conn_str = f"{os.environ.get('DB_TYPE', 'mariadb+pymysql')}://{os.environ.get('DB_USER', 'user')}:{os.environ.get('DB_PASSWORD', 'Password_123')}@{os.environ.get('DB_SERVER', '127.0.0.1')}/{os.environ.get('DB', 'AttendanceQRsrv')}?charset=utf8mb4&" # check_same_thread=False&
+        conn_str = f"{os.environ.get('DB_TYPE', 'mariadb+pymysql')}://{os.environ.get('DB_USER', 'user')}:{os.environ.get('DB_PASSWORD', 'Password_123')}@{os.environ.get('DB_SERVER', '127.0.0.1')}/{os.environ.get('DB', 'SurveyAppDB')}?charset=utf8mb4&" # check_same_thread=False&
     print(f"Подключение к базе данных по адресу {conn_str}")
 
     engine = sa.create_engine(conn_str, echo=False, pool_size=10, max_overflow=20)
